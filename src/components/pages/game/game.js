533e1c5a6e68007d41c287c9ac09416efe93d5c4
Shoxsz <paulomarcio18p1@gmail.com>
2020-02-11 20:47:02 -0300

    game page

---
+++ src/components/pages/game/game.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainTemplate from '../../templates/main-template/main-template'
+import TypeText from '../../type-text/type-text'
+
+const GamePage = () => {
+  return (
+    <MainTemplate>
+      <TypeText data={ "dadfdqdqfdwqfwqfe" } />
+    </MainTemplate>
+  )
+}
+
+export default GamePage
