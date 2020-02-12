ef1d15b7b1fc419ad920cdc558eb26983592471a
Shoxsz <paulomarcio18p1@gmail.com>
2020-02-11 21:13:57 -0300

    type-text text comparison

--- src/components/type-text/type-text.js
+++ src/components/type-text/type-text.js
@@ -3,13 +3,41 @@ import React from 'react'
 import './type-text.css'
 
 const TypeText = ({ data }) => {
+  const [typedData, setTypedData] = React.useState('')
+  const inputRef = React.createRef()
+
+  const handleChange = () => {
+    setTypedData(inputRef.current.value)
+  }
+
+  const handleKeyDown = (event) => {
+    console.log(event)
+  }
+
+  const renderData = () => {
+    const spans = typedData.split('').map((str, index) => {
+      const typedChar = typedData.charAt(index)
+      const correctChar = data.charAt(index)
+
+      if(correctChar !== typedChar){
+        return <span className="type-text__typed-wrong">{ correctChar }</span>
+      }
+
+      return <span className="type-text__typed-right">{ correctChar }</span>
+    })
+
+    return (
+      <div className="type-text__totype">
+        { spans }{ data.substr(typedData.length) }
+      </div>
+    )
+  }
+
   return (
     <div className="type-text">
-      <textarea>
-        { data }
-      </textarea>
-      <div className="__description">Type what you see in the textarea above</div>
-      <input type="text"/>
+      { renderData() }
+      <div className="type-text__description">Type what you see in the textarea above:</div>
+      <input ref={ inputRef } onKeyDown={ handleKeyDown } onChange={ e => handleChange(e) } type="text"/>
     </div>
   )
 }
