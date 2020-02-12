74f834bea824f965f4ae654ae7513887d10cefd1
Shoxsz <paulomarcio18p1@gmail.com>
2020-02-12 20:56:36 -0300

    random generator implemented

--- src/components/pages/game/game.js
+++ src/components/pages/game/game.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import MainTemplate from '../../templates/main-template/main-template'
 import TypeText from '../../type-text/type-text'
+import { RandomGenerator, ALL_SYMBOLS } from "../../texts/random/random";
 
 const GamePage = () => {
+  const [data, setData] = React.useState(() => {
+    const data = RandomGenerator({ symbols: [ ALL_SYMBOLS ] })()
+    console.log(data)
+    return data
+  })
+
   return (
     <MainTemplate>
-      <TypeText data={ "dadfdqdqfdwqfwqfe" } />
+      <TypeText data={ data } />
     </MainTemplate>
   )
 }
---
+++ src/components/texts/random/random.js
@@ -0,0 +1,65 @@
+export const LETTERS = 'abcdefghijklmnopqrstuvwxyz'.split('')
+export const LETTERS_CAP = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+export const NUMBERS = '0123456789'.split('')
+export const MATH_SYMBOLS = '+-/*=()[]{}%<>'.split('')
+export const PUNCTUATION_SYMBOLS = ',.;?!`´\'"~^¨'.split('')
+export const SPECIAL_SYMBOLS = '\'"!@#$%¨¬&*()-_+=§´`{[ª^~]}º|\\<,.>;:?/°'
+
+export const ALL_SYMBOLS = [...LETTERS, ...LETTERS_CAP, ...NUMBERS, ...MATH_SYMBOLS, ...PUNCTUATION_SYMBOLS, ...SPECIAL_SYMBOLS]
+
+export const RandomGenerator = (options) => {
+  return function(){
+    const symbols = options.symbols || [ALL_SYMBOLS]
+    const length = options.length || 500
+
+    //calculate symbol array weights
+    let weight = 0
+    const weighted_symbols = []
+    let non_weighted_symbols = []
+    for(let symbol of symbols){
+      if(!!symbol.weight){
+        weight += symbol.weight
+        weighted_symbols.push(symbol)
+      }else{
+        non_weighted_symbols = [ ...non_weighted_symbols, ...symbol ]
+      }
+    }
+
+    if(!weight){
+      weight = 1
+    }
+
+    //generate random symbols for the weighted symbols
+    const weighted_generated = []
+    let count_generated = 0
+    for(let weighted_symbol of weighted_symbols){
+      let count = (weighted_symbol.weight / weight) * length
+      const generated = []
+      while(count--){
+        const index = Math.ceil(Math.random() * (weighted_symbol.symbols.length - 1))
+        generated.push(weighted_symbol.symbols[index])
+      }
+
+      weighted_generated.push(generated)
+      count_generated += count
+    }
+
+    const non_weighted_generated = []
+    //generate the remaining symbols for the non weighted
+    while(count_generated++ < length){
+      const index = Math.ceil(Math.random() * (non_weighted_symbols.length - 1))
+      non_weighted_generated.push(non_weighted_symbols[index])
+    }
+
+    //merge & shuffle the arrays
+    const generated = [ ...non_weighted_generated, ...weighted_generated ]
+    for(let i = 0; i < generated.length; ++i){
+      const randomIndex = Math.ceil(Math.random() * (generated.length - 1))
+      const aux = generated[i]
+      generated[i] = generated[randomIndex]
+      generated[randomIndex] = aux
+    }
+
+    return generated.toString()
+  }
+}
