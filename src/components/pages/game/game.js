c1e29cb3d1655142945a1a8e8618cbdfe1fd8e0a
Shoxsz <paulomarcio18p1@gmail.com>
2020-02-14 19:41:13 -0300

    added overflow in type text div

--- src/components/pages/game/game.js
+++ src/components/pages/game/game.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import MainTemplate from '../../templates/main-template/main-template'
 import TypeText from '../../type-text/type-text'
-import { RandomGenerator, ALL_SYMBOLS } from "../../texts/random/random";
+import { RandomGenerator, ALL_SYMBOLS, LETTERS } from "../../texts/random/random";
 
 const GamePage = () => {
   const [data, setData] = React.useState(() => {
-    const data = RandomGenerator({ symbols: [ ALL_SYMBOLS ] })()
+    // const data = RandomGenerator({ symbols: [ ALL_SYMBOLS ] })()
+    const data = RandomGenerator( { symbols: [LETTERS], length: 2000 } )()
+    
     return data
   })
 
